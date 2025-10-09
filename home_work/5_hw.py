b8bce1a4525c81563427dc97f601eed73367b4a3
Olga Spiridonova <spiri.o.s@yandex.ru>
2025-10-09 22:04:13 +0300

    add 5_hw

--- home_work/5_hw.py
+++ home_work/5_hw.py
@@ -6,9 +6,10 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
-driver.get("https://www.saucedemo.com/")
+def open_site(url):
+    return driver.get(url)
+open_site("https://www.saucedemo.com/")
 
-# поиск элемента
 text_area_username = driver.find_element(By.CSS_SELECTOR, '#user-name')
 text_area_password = driver.find_element(By.CSS_SELECTOR, '#password')
 button_submit = driver.find_element(By.CSS_SELECTOR, '#login-button')
