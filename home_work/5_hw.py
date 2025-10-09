b60607a953028988be0e3535d5a290b72ccc3aa0
Olga Spiridonova <spiri.o.s@yandex.ru>
2025-10-09 22:29:25 +0300

    /

--- home_work/5_hw.py
+++ home_work/5_hw.py
@@ -4,18 +4,17 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 
 driver = webdriver.Chrome()
-def open_site(url):
-    return driver.get(url)
-open_site("https://www.saucedemo.com/")
-
-text_area_username = driver.find_element(By.CSS_SELECTOR, '#user-name')
-text_area_password = driver.find_element(By.CSS_SELECTOR, '#password')
-button_submit = driver.find_element(By.CSS_SELECTOR, '#login-button')
-if (text_area_username is None) and (text_area_password is None) and (button_submit is None):
-    print('Элементы не найдены')
-else:
-    print ('Элементы найдены')
-
+def open_site():
+    driver.get("https://www.saucedemo.com/")
+    try:
+        driver.find_element(By.CSS_SELECTOR, '#user-name')
+        driver.find_element(By.CSS_SELECTOR, '#password')
+        driver.find_element(By.CSS_SELECTOR, '#login-button')
+        print("Элементы найдены")
+    except NoSuchElementException:
+        print("Элементы не найдены")
+open_site()
 #все 3 элемента найдены
--- tests/test_2_open_site.py
+++ tests/test_2_open_site.py
@@ -1,10 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
 
 driver = webdriver.Chrome( )
 driver.get("https:/demoqa.com/")
 
 # поиск элемента
+time.sleep(5)
 icon = driver.find_element(By.CSS_SELECTOR, 'header > a > img')
 if icon is None:
     print('Элемент не найден')
